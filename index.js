6f69b55bf7548e16b5fc68b146080235d86c76f1
Begli0102 <begli.kaka@mail.ru>
2021-03-26 12:50:32 +0100

    developed app with express
    Added the express framework
    -Added the Morgan module
    -Added an error handling middleware function
    -Added a Public directory which is the default directory for static files

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const morgan= require('morgan');
+const app = express();
+
+
+
+let topTenMovies=[
+  {
+    title:'The Shawshank Redemption',
+    director: 'Frank Darabont',
+    year:1994
+  },
+
+  {
+    title:'The Godfather',
+    director: 'Francis Ford Coppola',
+    year:1972
+  },
+
+  {
+    title:'The Lord of the Rings: The Return of the King ',
+    director: 'Peter Jackson',
+    year:2003
+  },
+
+  {
+    title:'Forrest Gump ',
+    director:'Robert Zemeckis',
+    year:1994
+  },
+
+  {
+    title:'The Matrix ',
+    director:'Lana Wachowski, Lilly Wachowski',
+    year:1999
+  },
+
+  {
+    title:'The Green Mile',
+    director:'Frank Darabont',
+    year:1999
+  },
+
+  {
+    title:'The Pianist ',
+    director:'Roman Polanski',
+    year:2002
+  },
+
+  {
+    title:'Gladiator',
+    director:'Ridley Scott',
+    year:2000
+  },
+
+  {
+    title:'Joker',
+    director:'Todd Phillips',
+    year:2019
+  },
+
+  {
+    title:'Finding Nemo',
+    director:'Andrew Stanton, Lee Unkrich',
+    year:2003
+  },
+]
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the world of movies!');
+});
+
+app.use(express.static('public'));
+
+app.use(morgan('common'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
