d42f32586641dce57d6c02377411ad1fd01a5627
Begli0102 <begli.kaka@mail.ru>
2021-04-17 14:16:59 +0200

    added a validation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan= require('morgan');
 const bodyParser=require('body-parser');
 const passport=require('passport');
 require('./passport');
+const cors=require('cors');
+const { check, validationResult } = require('express-validator');
 
 
 const mongoose = require("mongoose");
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(morgan('common'));
 app.use(express.static('public'));
 let auth = require('./auth')(app);
+app.use(cors());
 
 app.get('/',(req, res)=>{
   res.send('Welcome to MyFlix!');
@@ -90,7 +93,17 @@ app.get('/users',passport.authenticate('jwt',{session:false}), (req, res) => {
 
 
 // Adding a new user
-app.post('/users',passport.authenticate('jwt',{session:false}),(req,res) => {
+app.post('/users',
+   [check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ],(req,res) => {
+    let errors=validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(422).json({errors:errors.array()});
+    }
+   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({Username:req.body.username})
   .then ((user) => {
     if (user) {
@@ -119,25 +132,45 @@ app.post('/users',passport.authenticate('jwt',{session:false}),(req,res) => {
 
 //Updating user's data
 
-app.put('/users/:username',passport.authenticate('jwt',{session:false}), (req, res) => {
-  Users.findOneAndUpdate({ Username: req.params.username }, { $set:
+app.put("/users/:username",
+[check('Username', 'Username is required').isLength({min: 5}),
+ check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+ check('Password', 'Password is required').not().isEmpty(),
+ check('Email', 'Email does not appear to be valid').isEmail()
+],(req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOneAndUpdate(
+        {
+            Username : req.params.username
+        },
+        {
+            $set:
+            {
+                Name: req.body.Name,
+                Username: req.body.Username,
+                Password: hashedPassword,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+            }
+        },
+        {
+            new: true
+        }
+    ).then((updatedUser) =>
     {
-      Username: req.body.username,
-      Password: req.body.password,
-      Email: req.body.email,
-      Birthday: req.body.birthday
-    }
-  },
-  { new: true }, // This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    } else {
-      res.json(updatedUser);
-    }
-  });
+        res.json(updatedUser)
+    }).catch((err) =>
+    {
+        console.log(err);
+        res.status(500).send("Error: " + err)
+    })
 });
+
+
 //Adding user a favourite movie
 app.post('/users/:username/movies/:movieId',passport.authenticate('jwt',{session:false}), (req, res) => {
   Users.findOneAndUpdate({ Username: req.params.username }, {
@@ -194,6 +227,7 @@ app.delete('/users/:username',passport.authenticate('jwt',{session:false}), (req
     });
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
