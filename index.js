2c15e78f50e5734119dba4bdafc10a91f3730448
Begli0102 <begli.kaka@mail.ru>
2021-06-04 15:01:47 +0200

    add code that adds a single user

--- index.js
+++ index.js
@@ -194,6 +194,17 @@ app.put("/users/:username",
     })
 });
 
+// Get a single user
+app.get('/users/:username', passport.authenticate('jwt',{session:false}), (req, res) => {
+  Users.findOne({ Username: req.params.username })
+   .then((user) => {
+     res.json(user);
+   })
+   .catch((err) => {
+     console.error(err);
+     res.status(500).send('Error: ' + err);
+   });
+});
 
 //Adding user a favourite movie
 app.put('/users/:username/movies/:movieId', passport.authenticate('jwt', {session: false}), ( req, res ) =>{ 
