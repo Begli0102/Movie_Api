37294a3730c82ccb2e3cdd55a8ffa276b62ff107
Begli0102 <begli.kaka@mail.ru>
2021-07-30 09:36:39 +0200

    update birthday format

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post('/users',
  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
  check('Password', 'Password is required').not().isEmpty(),
  check('Email', 'Email does not appear to be valid').isEmail(),
-check("Birthday", 'Birthday doesn\'t appear to be valid').isDate({format:"DD-MM-YYYY"})
+check("Birthday", 'Birthday doesn\'t appear to be valid').isDate({format:"YYYY-MM-DD"})
 ], (req, res) => {
 
 // check the validation object for errors
@@ -160,7 +160,7 @@ app.put("/users/:username",
  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
  check('Password', 'Password is required').not().isEmpty(),
  check('Email', 'Email does not appear to be valid').isEmail(),
- check("Birthday", 'Birthday doesn\'t appear to be valid').isDate({format: "DD-MM-YYYY"})
+ check("Birthday", 'Birthday doesn\'t appear to be valid').isDate({format: "YYYY-MM-DD"})
 ],(req, res) => {
     let errors = validationResult(req);
     if (!errors.isEmpty()) {
